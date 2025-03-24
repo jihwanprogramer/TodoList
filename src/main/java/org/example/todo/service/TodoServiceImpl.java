b1c23576c2aa80e0d6a54f36ba55de57052b1e45
kob882333@naver.com <kob882333@naver.com>
2025-03-24 20:46:01 +0900

    feat: TodoServiceImpl 구현 추가 TodoService 인터페이스를 구현하는 TodoServiceImpl 클래스를 추가하였습니다. - Todo 저장, 조회, 조건 검색 및 업데이트 기능을 포함.

--- src/main/java/org/example/todo/service/TodoServiceImpl.java
+++ src/main/java/org/example/todo/service/TodoServiceImpl.java
@@ -5,9 +5,13 @@ import org.example.todo.dto.TodoRequestDto;
 import org.example.todo.dto.TodoResponseDto;
 import org.example.todo.repository.TodoRepository;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class TodoServiceImpl implements TodoService{
@@ -22,7 +26,38 @@ public class TodoServiceImpl implements TodoService{
     @Override
     public TodoResponseDto saveTodo(TodoRequestDto requestDto) {
         Date currentDate = new Date();
-        Todo todo = new Todo(requestDto.getTodo(), requestDto.getName(), requestDto.getPassword(),currentDate);
+        Date editDate = new Date();
+        Todo todo = new Todo(requestDto.getTodo(), requestDto.getName(), requestDto.getPassword(),currentDate,editDate);
         return todoRepository.saveTodo(todo);
     }
+
+    @Override
+    public TodoResponseDto findTodoId(Long id) {
+        Todo todo = todoRepository.findTodoById(id);
+        return new TodoResponseDto(todo);
+    }
+
+    @Override
+    public List<TodoResponseDto> findConditionTodo(Date editDate, String name) {
+        return todoRepository.findConditionTodo(editDate, name);
+    }
+
+    @Override
+    public TodoResponseDto updateTodo(Long id, String todo, String name) {
+        Todo todos =todoRepository.findTodoById(id);
+
+        if (todo == null && name == null) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "값이 없다");
+        }
+
+        int updateRow = todoRepository.updateTodo(id,todo,name);
+
+        if(updateRow ==0){
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id);
+            }
+
+
+        return new TodoResponseDto(todos);
+    }
+
 }
