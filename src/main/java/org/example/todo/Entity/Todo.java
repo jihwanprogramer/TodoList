64fec89bce3dd4cdebfd7c66150090162c46b0b5
kob882333@naver.com <kob882333@naver.com>
2025-03-24 19:59:29 +0900

    feat: Todo.java에 다양한 생성자 추가
    Todo 엔티티에 괄호 안의 변수에 따라 다른 생성자를 추가하였습니다.
    - @Getter 및 @AllArgsConstructor를 사용하여 코드 간결화

--- src/main/java/org/example/todo/Entity/Todo.java
+++ src/main/java/org/example/todo/Entity/Todo.java
@@ -7,18 +7,27 @@ import java.util.Date;
 @Getter
 @AllArgsConstructor
 public class Todo {
+    private Long id;
     private String todo;
     private String name;
     private String password;
     private Date uploadDate;
     private Date editDate;
-    private Long id;
 
-    public Todo(String todo,String name,String password,Date uploadDate){
+    public Todo(String todo,String name,String password,Date uploadDate,Date editDate){
         this.todo = todo;
         this.name = name;
         this.password = password;
         this.uploadDate = uploadDate;
+        this.editDate = editDate;
+    }
+
+    public Todo(Long id,String todo,String name,Date uploadDate,Date editDate){
+        this.id = id;
+        this.todo = todo;
+        this.name = name;
+        this.uploadDate = uploadDate;
+        this.editDate = editDate;
     }
 }
 
