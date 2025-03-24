8b18ccd59de934b10f4f14df7f1353b11b4828c6
kob882333@naver.com <kob882333@naver.com>
2025-03-24 20:45:49 +0900

    feat: TodoService 인터페이스 추가 Todo 관련 비즈니스 로직을 정의하는 TodoService 인터페이스를 추가하였습니다.

--- src/main/java/org/example/todo/service/TodoService.java
+++ src/main/java/org/example/todo/service/TodoService.java
@@ -3,7 +3,17 @@ package org.example.todo.service;
 import org.example.todo.Entity.Todo;
 import org.example.todo.dto.TodoRequestDto;
 import org.example.todo.dto.TodoResponseDto;
+import org.springframework.http.ResponseEntity;
+
+import java.util.Date;
+import java.util.List;
 
 public interface TodoService {
     TodoResponseDto saveTodo(TodoRequestDto requestDto);
+
+    TodoResponseDto findTodoId(Long id);
+
+    List<TodoResponseDto> findConditionTodo(Date editDate, String name);
+
+    TodoResponseDto updateTodo(Long id, String todo, String name);
 }
