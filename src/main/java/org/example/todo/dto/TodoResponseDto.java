edb67e9a7899e3a35da48ed6b68678db9a497611
kob882333@naver.com <kob882333@naver.com>
2025-03-25 18:40:52 +0900

    modified: TodoResponseDto.java 주석 추가 및 코드 정리

--- src/main/java/org/example/todo/dto/TodoResponseDto.java
+++ src/main/java/org/example/todo/dto/TodoResponseDto.java
@@ -6,16 +6,43 @@ import org.example.todo.Entity.Todo;
 
 import java.util.Date;
 
-
+/**
+ * Todo 응답 데이터 전송 객체 (DTO).
+ */
 @Getter
 @AllArgsConstructor
 public class TodoResponseDto {
+
+    /**
+     * 식별자.
+     */
     private Long id;
+
+    /**
+     * 할 일.
+     */
     private String todo;
+
+    /**
+     * 이름.
+     */
     private String name;
+
+    /**
+     * 생성일.
+     */
     private Date uploadDate;
+
+    /**
+     * 수정일.
+     */
     private Date editDate;
 
+    /**
+     * Todo 객체를 기반으로 하는 TodoResponseDto 생성자.
+     *
+     * @param todo Todo 엔티티 객체
+     */
     public TodoResponseDto(Todo todo) {
         this.id = todo.getId();
         this.todo = todo.getTodo();
@@ -24,9 +51,3 @@ public class TodoResponseDto {
         this.editDate = todo.getEditDate();
     }
 }
-
-
-
-
-
-
