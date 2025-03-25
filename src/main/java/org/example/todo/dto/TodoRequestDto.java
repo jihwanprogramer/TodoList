f7b391c332a9ad42c501744dbedc1c0b7c3d7bc8
kob882333@naver.com <kob882333@naver.com>
2025-03-25 18:40:36 +0900

    modified: TodoRequestDto.java 주석 추가 및 코드 정리

--- src/main/java/org/example/todo/dto/TodoRequestDto.java
+++ src/main/java/org/example/todo/dto/TodoRequestDto.java
@@ -2,9 +2,21 @@ package org.example.todo.dto;
 
 import lombok.Getter;
 
+/**
+ * Todo 요청 Dto
+ */
 @Getter
 public class TodoRequestDto {
+    /**
+     * 할 일
+     */
     private String todo;
+    /**
+     * 이름
+     */
     private String name;
+    /**
+     * 비밀번호
+     */
     private String password;
 }
